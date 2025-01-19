bd697e75e3f42fde645e4cc441aa199c1c2e4f99
DynamiteBear20 <nandi.aparna@gmail.com>
2025-01-19 19:03:55 +0530

    Django Creation 2

---
+++ App/Scripts/dd.py
@@ -0,0 +1,41 @@
+page = {
+    'tag':'html',
+    'attributes':{
+        'lang':'en'
+    },
+    'children':[
+        {
+            'tag':'head',
+            'attributes': {},
+            'children':[
+                {
+                    'tag':'title',
+                    'attributes':{},
+                    'children':[
+                        'Hello World'
+                    ]
+                }
+            ]
+        },
+        {
+            'tag':'body',
+            'attributes':{},
+            'children':[
+                {
+                    'tag':'h1',
+                    'attributes':{},
+                    'children':[
+                        'Hello World'
+                    ]
+                },
+                {
+                    'tag':'p',
+                    'attributes':{},
+                    'children':[
+                        'This is a paragraph'
+                    ]
+                }
+            ]
+        }
+    ]
+}
