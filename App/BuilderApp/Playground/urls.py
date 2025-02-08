4a38eb795f29091d6fa12538e920969b69794c5d
DynamiteBear20 <.com>
2025-02-08 14:27:40 +0530

    Better

--- App/BuilderApp/Playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('playground/<str:name>', views.playground, name='members'),
-    path('playground/result/build',views.result,name='result'),
-    path('playground/result',views.results,name='results')
-]
